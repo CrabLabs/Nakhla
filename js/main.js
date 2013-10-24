a9f540de6a37df0a3b5d23c7db585f9fd2329861
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2013-10-24 18:02:31 -0200

    Merge

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 (function ($) {
 	"use strict";
+	
 	$(document).on("ready", function () {
-		$(".nav_btn").on("click", function (e) {
-			e.preventDefault();
-			$(".nav").toggleClass('visible');
-			$('.nav_btn').toggleClass('giro');
-		});
+		var bgImgs = ["img/bg/img_1.jpg", "img/bg/img_2.jpg"];
+		$("#home").css("background-image", "url(" + bgImgs[Math.floor(Math.random() * bgImgs.length)] + ")");		
 	});
+
+	$(".nav span").on("click", function (e) {
+		e.preventDefault();
+		$(".nav").toggleClass("visible");
+		$(".nav span").toggleClass("giro");
+	});
+
 }(jQuery));
