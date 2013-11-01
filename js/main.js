d371d910ab69af01511b256128ec37caa772b0ee
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2013-10-31 23:01:11 -0200

    Basic scroll interaction

--- js/main.js
+++ js/main.js
@@ -1,22 +1,42 @@
 (function ($) {
 	"use strict";
-	
+	var windowHeight,
+		bgImgs;
+
 	$(document).on("ready", function () {
-		var bgImgs = ["img/bg/img_1.jpg", "img/bg/img_2.jpg", "img/bg/img_3.jpg"];
+		bgImgs = ["img/bg/img_1.jpg", "img/bg/img_2.jpg", "img/bg/img_3.jpg"];
 		$("#home").css("background-image", "url(" + bgImgs[Math.floor(Math.random() * bgImgs.length)] + ")");
+	});
+
+	$(document, window).on("ready resize", function () {
+		windowHeight = $(window).height();
+		$(".step").height(windowHeight);
 	});
 
 	$(document).on("scroll", function (event) {
 		var scrollTop = $(document).scrollTop(),
 			scrollMod = scrollTop / ($(document).height() - 80) * 4;
 
-		$(".nav ul li").each(function (index) {
-			if (index === Math.floor(scrollMod)) {
-				$(this).addClass("active");
-			} else {
-				$(this).removeClass("active");
-			}
-		});
+		// $(".nav ul li").each(function (index) {
+		// 	if (index === Math.floor(scrollMod)) {
+		// 		$(this).addClass("active");
+		// 	} else {
+		// 		$(this).removeClass("active");
+		// 	}
+		// });
+
+		if (scrollTop > 300)
+			$("#logo").addClass("hide");
+		else
+			$("#logo").removeClass("hide");
+
+		// console.log(scrollTop);
+		// if (scrollTop > 2643) {
+		// 	$(".horizontal_steps").css({
+		// 		'top': 
+		// 	});
+		// }
+
 	});
 
 	$("a[href*=#]").on("click", function (e) {
@@ -35,22 +55,4 @@
 			}
 		}
 	});
-
-	var logo = false;
-
-	$(document).scroll(function() {
-	    if( $(this).scrollTop() > 300 ) {
-	        if( !logo ) {
-	            logo = true;
-	            $('#logo').addClass('hide');
-	        }
-	    } else {
-	        if( logo ) {
-	            logo = false;
-	            $('#logo').removeClass('hide');
-	        }
-	    }
-	});
 }(jQuery));
-
-
